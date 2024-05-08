6d185291e82339cac627da02ed331dfe935172b6
Andrew Lobban <andrewglobban@gmail.com>
2024-05-08 12:36:39 -0500

    Validate TextField inputs

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -46,12 +46,28 @@ class _AuthScreenState extends State<AuthScreen> {
                           keyboardType: TextInputType.emailAddress,
                           autocorrect: false,
                           textCapitalization: TextCapitalization.none,
+                          validator: (value) {
+                            if (value == null ||
+                                value.trim().isEmpty ||
+                                !value.contains('@')) {
+                              return 'Please enter a valid email address.';
+                            }
+
+                            return null;
+                          },
                         ),
                         TextFormField(
                           decoration: const InputDecoration(
                             labelText: 'Password',
                           ),
                           obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.trim().length < 6) {
+                              return 'Password must be atleast 6 characters long.';
+                            }
+
+                            return null;
+                          },
                         ),
                         const SizedBox(
                           height: 12,
