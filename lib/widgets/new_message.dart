dea96c5ba8e9656918f5f2a1fc0f76ed4d5c5134
Andrew Lobban <andrewglobban@gmail.com>
2024-05-21 12:31:36 -0500

    Refactor to close soft keyboard on send

--- lib/widgets/new_message.dart
+++ lib/widgets/new_message.dart
@@ -27,6 +27,10 @@ class _NewMessageState extends State<NewMessage> {
       return;
     }
 
+    FocusScope.of(context).unfocus();
+
+    _messageController.clear();
+
     final user = FirebaseAuth.instance.currentUser!;
     final userData = await FirebaseFirestore.instance
         .collection('users')
@@ -40,8 +44,6 @@ class _NewMessageState extends State<NewMessage> {
       'username': userData.data()!['username'],
       'userImage': userData.data()!['image_url'],
     });
-
-    _messageController.clear();
   }
 
   @override
