5b421db4e45f57fbdb6b8a90b4ef7827bec17019
Andrew Lobban <andrewglobban@gmail.com>
2024-05-21 12:11:55 -0500

    Update Chat screen with new widgets

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
+
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
 
@@ -22,7 +25,14 @@ class ChatScreen extends StatelessWidget {
         ],
       ),
       body: const Center(
-        child: Text('Logged in!'),
+        child: Column(
+          children: [
+            Expanded(
+              child: ChatMessages(),
+            ),
+            NewMessage(),
+          ],
+        ),
       ),
     );
   }
